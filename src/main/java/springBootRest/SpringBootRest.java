6b620ab5303b8c312da1d7f49668200af0b29032
Jude Huang <JudeHuang2004@gmail.com>
2017-08-16 13:30:21 -0500

    remove call args[0].

--- src/main/java/springBootRest/SpringBootRest.java
+++ src/main/java/springBootRest/SpringBootRest.java
@@ -9,7 +9,7 @@ public class SpringBootRest {
 
 	public static void main(String[] args) {
 //		System.getProperties().put("server.port", 8089);
-		System.out.println("---------------------------args[0]: "+args[0]);
+//		System.out.println("---------------------------args[0]: "+args[0]);
 		SpringApplication.run(SpringBootRest.class, args);
 	}
 }
